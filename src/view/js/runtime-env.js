97e3156d6695511823cd083d6da0bb9eb8460888
cadena0 <cadenajuanjose68@gmail.com>
2026-03-13 22:35:14 -0500

    arreglo de api y ruta a base de datos

--- src/view/js/runtime-env.js
+++ src/view/js/runtime-env.js
@@ -1,5 +1,5 @@
 // Runtime environment defaults for local usage.
 // This file is overwritten during the Vercel build step.
 window.__ENV__ = window.__ENV__ || {
-  SIGAM_API: ''
+  SIGAM_API: 'https://sigam-backend.vercel.app'
 };
